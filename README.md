3a07cfcd546e34f0cf6728c61a1abf6b96885358
JK <jkdesign@me.com>
2018-10-16 05:19:22 +0200

    Added images.
    Work on piGPIO

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # piTelex
 Control a historic Telex device with a Raspberry Pi in Python
+
+
+
+## Electronic
+
+### Overview of Modules
+
+![TelexOverview](TelexOverview.png)
+
+### Current Source and Regulator
+
+![TelexCurrent](TelexCurrent.png)
+
+### Telex Transmitter with Bipolar Transistor
+
+![TelexTXD](TelexTXD.png)
+
+### Telex Transmitter with FET Transistor
+
+![TelexTXD](TelexTXDFET.png)
+
+### Telex Receiver
+
+![TelexRXD|small](TelexRXD.png)
+
+
+
+## Software
+
+TODO...
--- TelexPiGPIO.py
+++ TelexPiGPIO.py
@@ -8,8 +8,11 @@ __copyright__   = "Copyright 2018, JK"
 __license__     = "GPL3"
 __version__     = "0.0.1"
 
+#https://www.programcreek.com/python/example/93338/pigpio.pi
+
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
 import MurrayCode
+import time
 
 pi = pigpio.pi()
 
@@ -32,7 +35,11 @@ class TelexPiGPIO:
         self._pin_dtr = pin_dtr
         self._invert = invert
         self._tx_buffer = []
+        self._rx_buffer = []
+        self._cb = None
+        self._pulse_dial_count = 0
 
+        # init GPIOs
         pi.set_pad_strength(0, 8)
         pi.set_mode(self._pin_rxd, pigpio.INPUT)
         pi.set_pull_up_down(self._pin_rxd, pigpio.PUD_UP)
@@ -43,12 +50,15 @@ class TelexPiGPIO:
         pi.set_mode(self._pin_dtr, pigpio.OUTPUT)
         pi.write(self._pin_dtr, 1)
 
+        # init bit bongo serial read
         pi.set_noise_filter(self._pin_rxd, 1000, 1000)   # 1ms, 1ms
         pi.bb_serial_invert(self._pin_rxd, self._invert)
         status = pi.bb_serial_read_open(self._pin_rxd, 50)   # 50 baud
 
+        # init bit bongo serial write
         self.last_wid = None
 
+        # debug
         cbs = pi.wave_get_max_cbs()
         micros = pi.wave_get_max_micros()
         pulses = pi.wave_get_max_pulses()
@@ -63,19 +73,22 @@ class TelexPiGPIO:
 
     def read(self) -> str:
         #if not self._tty.in_waiting:
-        #    return ''
+        ret = ''
 
         count, data = pi.bb_serial_read(self._pin_rxd)
-
-        if self._tx_buffer:
-            self._write_wave()
-
         if count:
             m = data
             a = self._mc.decode(m)
-            return a
+            ret += a
         
-        return ''
+        if self._tx_buffer:
+            self._write_wave()
+
+        if self._rx_buffer:
+            ret += self._rx_buffer
+            self._rx_buffer = []
+
+        return ret
 
 
     def write(self, a:str):
@@ -95,12 +108,12 @@ class TelexPiGPIO:
 
         #pi.wave_clear()
 
-        pi.wave_add_generic([pigpio.pulse(1<<self._pin_rts, 0, 10)]) # add dir pulse to waveform
+        pi.wave_add_generic([pigpio.pulse(1<<self._pin_rts, 0, 10)]) # add dir/rts pulse to waveform
 
-        # self._tx_buffer
+        #                                     self._tx_buffer
         pi.wave_add_serial(self._pin_txd, 50, [0, 0x1F, 0x15], 0, 5, 3)
 
-        pi.wave_add_generic([pigpio.pulse(0, 1<<self._pin_rts, 10)]) # add dir pulse to waveform
+        pi.wave_add_generic([pigpio.pulse(0, 1<<self._pin_rts, 10)]) # add dir/rts pulse to waveform
 
         new_wid = pi.wave_create() # commit waveform
         pi.wave_send_once(new_wid) # transmit waveform
@@ -115,23 +128,26 @@ class TelexPiGPIO:
         self._tx_buffer = []
 
 
-    def enable_pulses(self, enable:bool):
+    def enable_pulse_dial(self, enable:bool):
         if self._cb:
             self._cb.cancel()
             self._cb = None
 
         if enable:
-            self._cb = pi.callback(self._pin_rxd, pigpio.FALLING_EDGE, callback_pulses)
-            set_watchdog(self._pin_rxd, 300)   # 300ms
+            self._cb = pi.callback(self._pin_rxd, pigpio.FALLING_EDGE, self._callback_pulse_dial)
+            pi.set_watchdog(self._pin_rxd, 150)   # 150ms
         else:
-            set_watchdog(self._pin_rxd, 0)   # disable
+            pi.set_watchdog(self._pin_rxd, 0)   # disable
 
 
-    def _callback_pulses(self, gpio, level, tick):
+    def _callback_pulse_dial(self, gpio, level, tick):
         print(gpio, level, tick)
 
         if level == 2:   # watchdog timeout
-            pass
+            self._rx_buffer += str(self._pulse_dial_count)
+            self._pulse_dial_count = 0
+        else:
+            self._pulse_dial_count += 1
 
 #######
 
