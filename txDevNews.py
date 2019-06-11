f336e3f18774e2c8e82a51f87bb99f373a3cbd26
Benjamin Kunz <benjamin.kunz@gmail.com>
2019-06-11 23:40:01 +0200

    make path printing optional

--- txDevNews.py
+++ txDevNews.py
@@ -48,8 +48,9 @@ class TelexNews(txBase.TelexBase):
                         self._last_path = path
                         self._last_text = text
                         if text:
-                            path = path[:-4].replace('\\', '/')
-                            text = path + '\r\n' + '='*len(path) + '\r\n' + text
+                            if self._print_path:
+                                path = path[:-4].replace('\\', '/')
+                                text = path + '\r\n' + '='*len(path) + '\r\n' + text
                             self._news_buffer.append(text)
             except:
                 pass
@@ -66,7 +67,7 @@ class TelexNews(txBase.TelexBase):
         self.params = params
 
         self._newspath = params.get('newspath', './news')
-
+        self._print_path = self.params.get('print_path', False)
         self._rx_buffer = []
         self._news_buffer = []
         self._state_counter = 1
