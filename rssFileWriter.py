9c00636c20ef2f6768b2e9493b7ae1cb94ff39bd
Benjamin Kunz <benjamin.kunz@gmail.com>
2019-06-11 23:10:26 +0200

    add a feature to prepend visible names to entry titles

--- rssFileWriter.py
+++ rssFileWriter.py
@@ -13,7 +13,7 @@ h.ignore_images = True
 h.ignore_emphasis = True
 h.ignore_tables = True
 
-def formatted_write(output_path, rss_entry):
+def formatted_write(output_path, rss_entry, visible_name):
     #outfilenames = glob(output_path + "*.rsstx")
     #outfilename = output_path + "entry-%d.rsstx" % (len(outfilenames) + 1)
     outfilename = output_path + "NEWS-" + time.strftime("%Y-%m-%dT%H%M%S", time.localtime(calendar.timegm(rss_entry.published_parsed))) + ".rsstx"
@@ -21,9 +21,9 @@ def formatted_write(output_path, rss_entry):
         summary = h.handle(rss_entry.summary)
         summary = summary.replace('\n', ' ')
         print(outfilename)
-        print(rss_entry.title) 
+        print(visible_name + rss_entry.title) 
         print("    " + summary + "\n")
-        outfile.write(rss_entry.title + "\n")
+        outfile.write(visible_name + rss_entry.title + "\n")
         outfile.write("    " + summary )
 
 if __name__ == "__main__":
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--timeout", type=int, default=10, help="timeout(seconds) between fetching feed updates")
     parser.add_argument("-f", "--forget-past", dest="forget_past", action='store_true', help="Don't look back")
     parser.add_argument("-p", "--ouput-path", dest="output_path", default="./", help="Output files path")
+    parser.add_argument("-n", "--visible-name", dest="visible_name", default="", help="Prepend this string to each heading")
 
     argv = sys.argv[1:]
     argp = parser.parse_args(argv)
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 
     if not argp.forget_past:
         for entry in feed.entries:
-            formatted_write(argp.output_path, entry)
+            formatted_write(argp.output_path, entry, argp.visible_name)
 
     known_ids = [entry.id for entry in feed.entries]
 
@@ -54,5 +55,5 @@ if __name__ == "__main__":
         if new_ids is not None:
             entries = filter(lambda x: x.id in new_ids, feed.entries)
             for entry in entries:
-                formatted_write(argp.output_path, entry)
+                formatted_write(argp.output_path, entry, argp.visible_name)
             known_ids = known_ids + list(new_ids)
