b00d96576e221cc29af608d49e27dd2577a9ef33
Benjamin Kunz <benjamin.kunz@gmail.com>
2019-06-13 20:41:48 +0200

    add an option to insert linebreaks into output after n characters

--- rssFileWriter.py
+++ rssFileWriter.py
@@ -13,18 +13,29 @@ h.ignore_images = True
 h.ignore_emphasis = True
 h.ignore_tables = True
 
-def formatted_write(output_path, rss_entry, visible_name):
+def split_newline_after(string, numchars):
+    lines = []
+    if numchars < 1:
+        return string
+    for i in range(0, len(string), numchars):
+        lines.append(string[i:i+numchars])
+    return '\n'.join(lines)
+
+def formatted_write(output_path, rss_entry, visible_name, split_lines):
     #outfilenames = glob(output_path + "*.rsstx")
     #outfilename = output_path + "entry-%d.rsstx" % (len(outfilenames) + 1)
     outfilename = output_path + "NEWS-" + time.strftime("%Y-%m-%dT%H%M%S", time.localtime(calendar.timegm(rss_entry.published_parsed))) + ".rsstx"
     with open(outfilename, "a+") as outfile:
         summary = h.handle(rss_entry.summary)
         summary = summary.replace('\n', ' ')
-        print(outfilename)
-        print(visible_name + rss_entry.title) 
-        print("    " + summary + "\n")
-        outfile.write(visible_name + rss_entry.title + "\n")
-        outfile.write("    " + summary + "\n")
+        heading = visible_name + rss_entry.title
+        heading = split_newline_after(heading, split_lines)
+        summary = "    " + summary
+        summary = split_newline_after(summary, split_lines)
+        outfile.write(heading + "\n")
+        outfile.write(summary + "\n")
+        print(heading)
+        print(summary)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Scan an RSS feed and write the summy to files in a telex-friendly format")
@@ -34,6 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--forget-past", dest="forget_past", action='store_true', help="Don't look back")
     parser.add_argument("-p", "--ouput-path", dest="output_path", default="./", help="Output files path")
     parser.add_argument("-n", "--visible-name", dest="visible_name", default="", help="Prepend this string to each heading")
+    parser.add_argument("-s", "--split-lines", type=int, dest="split_lines", default=60, help="Insert a linebreak after n characters")
 
     argv = sys.argv[1:]
     argp = parser.parse_args(argv)
@@ -42,7 +54,7 @@ if __name__ == "__main__":
 
     if not argp.forget_past:
         for entry in feed.entries:
-            formatted_write(argp.output_path, entry, argp.visible_name)
+            formatted_write(argp.output_path, entry, argp.visible_name, argp.split_lines)
 
     known_ids = [entry.id for entry in feed.entries]
 
@@ -55,5 +67,5 @@ if __name__ == "__main__":
         if new_ids is not None:
             entries = filter(lambda x: x.id in new_ids, feed.entries)
             for entry in entries:
-                formatted_write(argp.output_path, entry, argp.visible_name)
+                formatted_write(argp.output_path, entry, argp.visible_name, argp.split_lines)
             known_ids = known_ids + list(new_ids)
