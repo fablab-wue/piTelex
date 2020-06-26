eaaa031573be647c0f56ad51d9ece30889727fdd
Björn Schließmann <9566675+b-schliessmann@users.noreply.github.com>
2020-06-26 19:05:24 +0200

    telex.py: Create global error log
    Thus far, exceptions were caught in a nonstandardised way and at least
    some exceptions' names were output onto the telex console (those caught
    and LOG()ged manually). This is nice for console users. But only if the
    exception is caught and logged properly, and if the debug level is set
    properly in txConfig.json.
    
    So we set up a global exception log to register all exceptions for later
    perusal. Everything is logged into a file rotated monthly.
    
    Direct logging from modules is also possible and will be set up in the
    next commit.
    
    Also, clean up unused import.

--- telex.py
+++ telex.py
@@ -11,9 +11,14 @@ __version__     = "0.0.1"
 import txConfig
 import txDevMCP
 
-import time
+import time, datetime
 import threading
-from argparse import ArgumentParser
+import os, os.path
+import sys
+import logging
+l = logging.getLogger("piTelex." + __name__)
+import logging.handlers
+import traceback
 
 #######
 # definitions and configuration
@@ -25,12 +30,102 @@ from argparse import ArgumentParser
 DEVICES = []
 TIME_20HZ = time.time()
 TIME_DELAY = None
+ERRLOG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "error_log")
+ERRLOG_LEVEL = logging.INFO
 
 #######
 # -----
 
 
 # =====
+
+class MonthlyRotatingFileHandler(logging.handlers.RotatingFileHandler):
+    """
+    Custom Handler for a monthly rotated log file. Implementation based on
+    original Python source code (CPython's Lib/logging/handlers.py).
+    """
+    def __init__(self, filename, mode='a', encoding=None):
+        # Disable maxBytes to ensure rolling over only on month change
+        # Disable dbackupCount because it's not used
+        # Disable delay to simplify overridden methods
+        super().__init__(filename, mode=mode, maxBytes=0, backupCount=0, encoding=encoding, delay=False)
+
+        # Initialise last year-month-stamp
+        self.last_year_month = datetime.datetime.now().strftime("%Y-%m")
+
+    def shouldRollover(self, record):
+        current_year_month = datetime.datetime.now().strftime("%Y-%m")
+        if self.last_year_month == current_year_month:
+            return 0
+        else:
+            return 1
+
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        dfn = self.rotation_filename(self.baseFilename + "_" + self.last_year_month)
+        self.last_year_month = datetime.datetime.now().strftime("%Y-%m")
+        if os.path.exists(dfn):
+            os.remove(dfn)
+        self.rotate(self.baseFilename, dfn)
+        self.stream = self._open()
+
+    def rotate(self, source, dest):
+        if os.path.exists(source):
+            os.rename(source, dest)
+
+def init_error_log():
+    """
+    Initialise error logging, i.e. create the root logger.
+
+    This is different from the log module, which implements a communication
+    trace log (i.e. it logs the data read from all piTelex modules).
+
+    Install handlers for uncaught exceptions.
+
+    All piTelex modules should initialise their logging like so:
+
+    >>> import logging
+    >>> l = logging.getLogger("piTelex." + __name__)
+
+    Calling l.warning et al. funnels all messages into the same log file of the
+    root logger ("piTelex").
+    """
+    logger = logging.getLogger("piTelex")
+    logger.setLevel(ERRLOG_LEVEL)
+    try:
+        os.mkdir(ERRLOG_PATH)
+    except FileExistsError:
+        pass
+    handler = MonthlyRotatingFileHandler(filename = os.path.join(ERRLOG_PATH, "piTelex-errors.log"))
+
+    handler.setLevel(ERRLOG_LEVEL)
+    formatter = logging.Formatter('%(asctime)s %(name)s [%(levelname)s]: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    sys.excepthook = excepthook
+    sys.unraisablehook = unraisablehook # Works from Python 3.8
+    threading.excepthook = threading_excepthook
+
+    # Log application start
+    from txDevLog import find_rev
+    rev = "(ERR)"
+    try:
+        rev = find_rev()
+    finally:
+        logger.info("===== piTelex rev " + rev)
+
+def excepthook(etype, value, tb):
+    l.critical("".join(traceback.format_exception(etype, value, tb)))
+
+def unraisablehook(unraisable):
+    excepthook(unraisable.exc_type, unraisable.exc_value, unraisable.exc_traceback)
+
+def threading_excepthook(args):
+    l.critical("Exception in Thread {}".format(args.thread))
+    excepthook(args.exc_type, args.exc_value, args.exc_traceback)
 
 def init():
     global DEVICES
@@ -117,6 +212,7 @@ def exit():
         device.exit()
         del device
     DEVICES = []
+    logging.shutdown()
 
 # =====
 
@@ -156,6 +252,7 @@ def main():
     txConfig.load()
     
     #test()
+    init_error_log()
     init()
 
     print('\033[2J-=TELEX=-')
