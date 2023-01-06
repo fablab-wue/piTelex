b3d4e7952c657142889742d1ee6b130b83f811b4
JK <jk@nerd2nerd.org>
2023-01-06 09:02:27 +0100

    fixed startup condition of power pin and number switch

--- txDevRPiTTY.py
+++ txDevRPiTTY.py
@@ -72,7 +72,7 @@ class TelexRPiTTY(txBase.TelexBase):
         self._inv_relay = params.get('inv_relay', False)
         self._pin_power = params.get('pin_power', 0)
         self._inv_power = params.get('inv_power', False)
-        self._pin_number_switch = params.get('pin_number_switch', params.get('pin_fsg_ns', 6))   # pin typical wired to rxd pin
+        self._pin_number_switch = params.get('pin_number_switch', params.get('pin_fsg_ns', 6 if self._mode not in ('V10', 'TWM') else 0))   # pin typical wired to rxd pin
         self._inv_number_switch = params.get('inv_number_switch', False)
 
         if self._inv_rxd:
@@ -136,7 +136,7 @@ class TelexRPiTTY(txBase.TelexBase):
 
         # init state
 
-        self._set_state(S_OFFLINE)
+        self._set_state(S_SLEEPING)
 
         # debug
         #cbs = pi.wave_get_max_cbs()
