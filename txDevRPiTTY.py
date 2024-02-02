5ca5fb8693d2ec45b3c0079bd76534bb227346c4
Rolf Obrecht <rolf.obrecht@web.de>
2024-02-02 14:41:28 +0100

    stop loop current in 'ZZ'/move pwr relay > RPiCtrl

--- txDevRPiTTY.py
+++ txDevRPiTTY.py
@@ -70,8 +70,6 @@ class TelexRPiTTY(txBase.TelexBase):
         self._inv_rxd = params.get('inv_rxd', False)
         self._pin_relay = params.get('pin_relay', 22)
         self._inv_relay = params.get('inv_relay', False)
-        self._pin_power = params.get('pin_power', 0)
-        self._inv_power = params.get('inv_power', False)
         self._pin_number_switch = params.get('pin_number_switch', 6 if self._mode not in ('V10', 'TWM', 'AGT-TWM') else 0)   # pin typical wired to rxd pin
         self._inv_number_switch = params.get('inv_number_switch', False)
 
@@ -107,13 +105,8 @@ class TelexRPiTTY(txBase.TelexBase):
             self._number_switch = NumberSwitch(self._pin_number_switch, self._callback_number_switch, self._inv_number_switch)
 
         pi.set_mode(self._pin_txd, pigpio.OUTPUT)
-        #pi.write(self._pin_txd, not self._inv_txd)
-        if self._pin_power:
-            pi.set_mode(self._pin_power, pigpio.OUTPUT)
-            pi.write(self._pin_power, self._inv_power)
-            pi.write(self._pin_txd, 0)
-        else:
-            pi.write(self._pin_txd, 1)
+        pi.write(self._pin_txd, 1)
+
         if self._pin_relay:
             pi.set_mode(self._pin_relay, pigpio.OUTPUT)   # relay for commutating
             pi.write(self._pin_relay, self._inv_relay)   # pos polarity
@@ -388,12 +381,10 @@ class TelexRPiTTY(txBase.TelexBase):
     # -----
 
     def _enable_power(self, enable:bool):
-        ''' set GPIO for the power SSR '''
-        if self._pin_power:
             l.debug('enable_power {}'.format(enable))
-            pi.write(self._pin_power, enable != self._inv_power)   # pos polarity
-            pi.write(self._pin_txd, enable)   # loop current on / off
-
+            if self._mode not in ('V10',):            # V.10 has no current loop, more modes affected? ('AGT...')
+                pi.write(self._pin_txd, enable)       # loop current on / off
+ 
     # -----
 
     def _set_time_squelch(self, t_diff:float):
