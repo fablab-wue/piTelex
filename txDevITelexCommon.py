8a37b87fe36c16e561d3ecc7c718acb76438c81a
Rolf Obrecht <74686345+rwobrecht@users.noreply.github.com>
2026-02-18 12:08:13 +0100

    Re-apply patch from coffeinator (commit 514c133) which was deleted by accident

--- txDevITelexCommon.py
+++ txDevITelexCommon.py
@@ -371,7 +371,7 @@ class TelexITelexCommon(txBase.TelexBase):
 
             try:
                 data = s.recv(1)
-
+                
                 # piTelex terminates; close connection
                 if not self._run:
                     break
@@ -624,14 +624,18 @@ class TelexITelexCommon(txBase.TelexBase):
                             self._rx_buffer.append(a)
                             self._received_counter += 1
 
-            except socket.timeout:
+            except socket.timeout: 
                 #l.debug('.')
                 if is_server and self.printer_start_timed_out:
                     self.printer_start_timed_out = False
-                    if is_ascii:
-                        s.sendall(b"der")
-                    else:
-                        self.send_reject(s, "der")
+                    try: # connection can be closed at this point
+                        if is_ascii:
+                            s.sendall(b"der")
+                        else:
+                            self.send_reject(s, "der")
+                    except:
+                            error = True
+                            break
                     l.error("Disconnecting client because printer didn't start up")
                     error = True
                     break
@@ -647,7 +651,11 @@ class TelexITelexCommon(txBase.TelexBase):
                         if (timeout_counter % 5) == 0:   # every 1 sec
                             # Send Acknowledge if printer is running
                             if self._connected >= ST.CON_FULL:
-                                self.send_ack(s, self._acknowledge_counter)
+                                try: # connection can be closed at this point
+                                    self.send_ack(s, self._acknowledge_counter)
+                                except:
+                                    error = True
+                                    break
 
                         if self._tx_buffer:
                             if time_next_send and time.monotonic() < time_next_send:
@@ -679,7 +687,12 @@ class TelexITelexCommon(txBase.TelexBase):
                             # character jumble.
 
 
-            except socket.error:
+            except ConnectionResetError:
+                l.warning("Connection reset by remote")
+                error = True
+                break
+
+            except (socket.error,BrokenPipeError):
                 l.error("Exception caught:", exc_info = sys.exc_info())
                 error = True
                 break
